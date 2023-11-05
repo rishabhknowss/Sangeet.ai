a8873e521a0cc9f7bba64459ed8859c7505c3a74
rishabhknowss <rishabhrai1515@gmail.com>
2023-11-05 09:32:34 +0530

    added favicon

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
       <main className="flex font-space overflow-hidden relative bg-black">
+        <h1 className="text-white absolute text-3xl left-1/2 translate-x-[-50%] font-bold">Sangeet AI</h1>
         <section className="flex flex-col-reverse w-full min-h-screen md:max-h-screen md:flex-row">
             <HomePage />
         </section>
